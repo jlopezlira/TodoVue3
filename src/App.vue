2305eafe85d9381b1b0e3d1804712cb2d084a998
JJ <jlopezlira@gmail.com>
2023-11-14 00:25:16 -0500

    Initial Setup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
     import { RouterView } from 'vue-router'
-    import MainHeader from './components/MainHeader.vue';
-    import MainFooter from './components/MainFooter.vue';
+    import MainHeader from '@/components/MainHeader.vue';
+    import MainFooter from '@/components/MainFooter.vue';
 </script>
 
 <template>
